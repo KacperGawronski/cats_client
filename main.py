6b8fcd689e88331a6ef7cd66e39e7142d1d99123
Kacper Gawronski <kacper.gawronski.public@gmail.com>
2019-01-17 22:19:05 +0100

    Added exception handling

--- main.py
+++ main.py
@@ -19,13 +19,15 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import socket
+import os
 
 #path is type of observer that makes path entry and filechooser button have same value
 class Path:
 	def __init__(self):
 		self.__value=''
+		self.__widgets=[]
 	def add_observable(self,widgets):
-		self.__widgets=widgets
+		self.__widgets.extend(widgets)
 	def set_path(self,path):
 		self.__value=path
 		for w in self.__widgets:
@@ -55,47 +57,63 @@ class PathEntry:
         self.__widget.set_text(path)
 
 class SendButton:
-    def __init__(self,widget,path,connection_details,response_label):
+    def __init__(self,widget,path,address_widget,port_widget,response_label):
         self.__widget=widget
         self.__widget.connect('clicked',self.__clicked)
         self.__path=path
-        self.__connection_details=connection_details
+        self.__address_widget=address_widget
+        self.__port_widget=port_widget
         self.__response_label=response_label
     def __clicked(self,widget):
-        with open(self.__path.get_path(),'rb') as image_file:
-            with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as connection_socket:
-                connection_socket.connect(self.__connection_details)
-                chunk=image_file.read(1024)
-                while(chunk):
-                    connection_socket.send(chunk)
-                    chunk=image_file.read(1024)
-                connection_socket.shutdown(socket.SHUT_WR)
-                chunk=connection_socket.recv(1024)
-                response=''
-                while(chunk):
-                    response+=chunk.decode('ascii')
-                    chunk=connection_socket.recv(1024)
-                self.__response_label.set_text(response)
+        if os.path.exists(self.__path.get_path()):
+            with open(self.__path.get_path(),'rb') as image_file:
+                with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as connection_socket:
+                    try:
+                        connection_details=(self.__address_widget.get_text(),int(self.__port_widget.get_text()))
+                        connection_socket.connect(connection_details)
+                    except Exception as e:
+                        self.__response_label.set_text('Connection error.')
+                    finally:
+                        chunk=image_file.read(1024)
+                        while(chunk):
+                             connection_socket.send(chunk)
+                             chunk=image_file.read(1024)
+                        connection_socket.shutdown(socket.SHUT_WR)
+                        chunk=connection_socket.recv(1024)
+                        response=''
+                        while(chunk):
+                            response+=chunk.decode('ascii')
+                            chunk=connection_socket.recv(1024)
+                        self.__response_label.set_text(response)
+        else:
+            self.__response_label.set_text('Error reading file.')
 class MainApp:
+    def __show_server_menu(self,widget):
+        self.__server_menu_window.show_all()
+    def __hide_server_menu(self,widget):
+        self.__server_menu_window.hide()
+        return False
     def __init__(self):
-        self.__server_address='192.168.122.201'
-        self.__port=9500
 
-        builder=Gtk.Builder()
-        builder.add_from_file("GUI.glade")
+        self.builder=Gtk.Builder()
+        self.builder.add_from_file("GUI.glade")
 
         self.__path=Path()
+        
+        self.__window=self.builder.get_object("main_window")
+        self.__window.connect("destroy",Gtk.main_quit)
+        
+        self.__server_address=self.builder.get_object("server_address")
+        self.__server_port=self.builder.get_object("server_port")
+        
+        self.__response_label=self.builder.get_object('response')
+        self.__send_button=SendButton(self.builder.get_object('send'),self.__path,self.__server_address,self.__server_port,self.__response_label)
 
-        self.__window=builder.get_object("main_window")
-                
-        self.__response_label=builder.get_object('response')
-        self.__send_button=SendButton(builder.get_object('send'),self.__path,(self.__server_address,self.__port),self.__response_label)
-
-        self.__path_entry=PathEntry(builder.get_object("search_path"),self.__path)
-        self.__file_chooser_button=PathButton(builder.get_object("file_chooser"),self.__path)
+        self.__path_entry=PathEntry(self.builder.get_object("search_path"),self.__path)
+        self.__file_chooser_button=PathButton(self.builder.get_object("file_chooser"),self.__path)
 		
         self.__path.add_observable([self.__path_entry,self.__file_chooser_button])
-		
+        
     def show_all(self):
          self.__window.show_all()
 MA=MainApp()
